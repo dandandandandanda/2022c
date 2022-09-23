0c6a9f54ba199dedc6bb5a9b4c35754eef771a16
dandandandandanda <114201562+dandandandandanda@users.noreply.github.com>
2022-09-23 12:00:23 +0800

    week02

---
+++ week02-2.c
@@ -0,0 +1,12 @@
+///file-new-empyt 空白檔案(滾輪放大)
+///file-save file as另存week02-1.cpp 會變色
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    a=9;
+    printf("現在的a是%d",a);
+    a=77;
+    printf("現在的a是%d",a);
+}
