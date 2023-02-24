aa9b476c3525c88c751964f58fe2c4b3c87d7377
dandandandandanda <deepnine1013@gmail.com>
2023-02-24 11:50:39 +0800

    第二週的程式

---
+++ week02/week02-4.cpp
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+    long long int a, b;
+
+    while( std::cin >> b){
+        long long int ans a - b;
+        if(ans<0) ans = b - a;
+        std::cout << ans << std::endl;
+    }
+}
