5963536193a9154b6e420b5c53160dd21ef7505e
dandandandandanda <deepnine1013@gmail.com>
2023-03-17 12:01:32 +0800

    week05

---
+++ week05/week05-2.cpp
@@ -0,0 +1,22 @@
+
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+
+    //    x:1234567   r:0
+    //               7     7 = 0*10 +7
+    //              6     76 =7*10 +6
+    //             5     765 = 76*10+4
+    //            4     7654 = 765*10+4
+
+    int r = 0, x2=x;
+    while(x>0){
+       r = r*10 + x%10;
+       //printf("x:%d r:%d\n", x, r);
+
+       x = x /10;
+
+       }
+       if(r==x2) return true;
+       else return false;
+
+};
